a51c8dc71d560291b72f9f0ca2f4855da3fc60f5
Demostenes777 <alaitz.mendiola@gmail.com>
2018-01-16 22:13:51 +0000

    Alphabet soup algorithm implemented and tested

--- src/AlphabetSoup.java
+++ src/AlphabetSoup.java
@@ -1,8 +1,30 @@
+import java.util.Arrays;
+
 public class AlphabetSoup {
 
     public AlphabetSoup() {}
 
     public boolean isPossible(String message, String letters) {
+        // sort message
+        String[] sortedMessage = message.split("");
+        Arrays.sort(sortedMessage, String.CASE_INSENSITIVE_ORDER);
+
+        // sort letters
+        String[] sortedLetters = letters.split("");
+        Arrays.sort(sortedLetters, String.CASE_INSENSITIVE_ORDER);
+
+        // check
+        int i = 0;
+        for (int j = 0; j < sortedLetters.length; j++) {
+            if (sortedMessage[i].equals(sortedLetters[j])) {
+                if (i == sortedMessage.length - 1) {
+                    return true;
+                } else {
+                    i++;
+                }
+            }
+        }
+
         return false;
     }
 }
--- test/AlphabetSoupTest.java
+++ test/AlphabetSoupTest.java
@@ -5,7 +5,24 @@ public class AlphabetSoupTest {
 
     @Test
     public void testIsPossible() {
+
         AlphabetSoup alphabetSoup = new AlphabetSoup();
+
+        Assert.assertTrue(alphabetSoup.isPossible("a", "a"));
         Assert.assertFalse(alphabetSoup.isPossible("a", "b"));
+        Assert.assertTrue(alphabetSoup.isPossible("a", "ab"));
+        Assert.assertTrue(alphabetSoup.isPossible("ab", "ab"));
+        Assert.assertFalse(alphabetSoup.isPossible("ab", "bc"));
+        Assert.assertTrue(alphabetSoup.isPossible("ab", "aab"));
+        Assert.assertTrue(alphabetSoup.isPossible("ab", "abc"));
+        Assert.assertFalse(alphabetSoup.isPossible("ab", "cdb"));
+        Assert.assertTrue(alphabetSoup.isPossible("hello", "ahbelol"));
+        Assert.assertFalse(alphabetSoup.isPossible("hello", "ahbeol"));
+        Assert.assertFalse(alphabetSoup.isPossible("HelloWorld", "ahlbeol"));
+        Assert.assertTrue(alphabetSoup.isPossible("HelloWorld", "HahlbeolWorld"));
+        Assert.assertTrue(alphabetSoup.isPossible("abc", "aabbcc"));
+        Assert.assertTrue(alphabetSoup.isPossible("c", "aabbcc"));
+        Assert.assertTrue(alphabetSoup.isPossible("d", "aabbccd"));
+
     }
 }
